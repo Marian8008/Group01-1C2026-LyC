8a092fc3088c0372ef2bd85d9cd873c25588ed87
Santiago Menendez <santiagomenendez@outlook.com>
2026-05-26 20:16:28 -0300

    Add exceptions and semantic validations

---
+++ src/main/java/lyc/compiler/model/InvalidOperandException.java
@@ -0,0 +1,7 @@
+package lyc.compiler.model;
+
+public class InvalidOperandException extends CompilerException {
+    public InvalidOperandException(String message) {
+        super(message);
+    }
+}
